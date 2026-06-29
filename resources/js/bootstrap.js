99cc8cf1844f8661fd55550caabc1dc148fa52dd
jhanslarico11 <jhanslarico11@gmail.com>
2026-06-29 15:29:39 -0500

    Corrige HTTPS en Render

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
 window.axios = axios;
 
+window.axios.defaults.baseURL = window.location.origin;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
